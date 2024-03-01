2c2921db0e4df366fb49ed77bd98663e1d7b6277
Yogev Kleinberger <t-yogevk@microsoft.com>
2024-03-01 22:00:08 +0200

    eval + improv

--- Assignment1/ass2.py
+++ Assignment1/ass2.py
@@ -21,6 +21,10 @@ reversed_map_all_poss_states = {state: i for i, state in enumerate(all_possible_
 visits_per_state = np.zeros(state_space_size)
 Vs = np.zeros(state_space_size)
 env = gym.make('Blackjack-v1', natural=False, sab=False)
+pi = np.zeros(state_space_size)
+
+relevent_states = [(i, j, k) for i in range(10,22) for j in range(1,11) for k in
+                        agent_has_ace_possible_values]
 
 def update_Vs(states,Gt):
     for state in states:
@@ -40,6 +44,14 @@ def state_reward(state,action):
             return -1
         else: return 0
 
+def choose_policy(state):
+    state = reversed_map_all_poss_states[state]
+    if pi[state] == 0:
+        choise =random.choice([0,1]) 
+        return choise
+    elif pi[state] ==1 : return 1
+    else: return 0
+
 def policy_eval(repeats):
     for i in range(repeats):
         observation, info = env.reset()
@@ -48,7 +60,11 @@ def policy_eval(repeats):
         terminated = False
         while(not terminated):
             # make action a from state s, and get new observation (s')
-            action = env.action_space.sample()  # agent policy that uses the observation and info
+            best_policy = random.randrange(0,1)
+            if best_policy<0.7:
+                action = choose_policy(observation)  
+            else:
+                action = 1- choose_policy(observation)
             if action == 0:
                 my_cards, dealer ,acc = observation
                 ace = 0
@@ -78,39 +94,63 @@ def policy_eval(repeats):
         update_Vs(states,Gt)
         
     env.close()
+
+
+def improve_policy(state):
+    agent, dealer, ace = state
+    stay = Vs[reversed_map_all_poss_states[(agent,dealer,ace)]]
+    hit = 0
+    for i in range(2,10):
+        hit += Vs[reversed_map_all_poss_states[(agent+i,dealer,ace)]]
+    hit += Vs[reversed_map_all_poss_states[(agent+11,dealer,True)]]*3
+    if stay>hit: pi[reversed_map_all_poss_states[state]] = -1
+    else: pi[reversed_map_all_poss_states[state]] = 1
+    
+    
+def learn(repets,improv):
+    for i in range(improv):
+        policy_eval(repets)
+        
+        for state in relevent_states:
+            improve_policy(state)
     
 if __name__ == '__main__':
-    # learn Transition matrix
-
-     
-    policy_eval(100000)
-    value = np.zeros((11,11))
+    learn(1000,1000)
+    value_pi = np.zeros((11,11))
+    value_Vs = np.zeros((11,11))
     for agent in range(11,22):
         for dealer in range(1,12):
-            value[agent-10][dealer-1] = Vs[reversed_map_all_poss_states[(agent,dealer,0)]]
+            value_Vs[agent-11][dealer-1] = Vs[reversed_map_all_poss_states[(agent,dealer,0)]]
+
+    for agent in range(11,22):
+        for dealer in range(1,12):
+            value_pi[agent-11][dealer-1] = pi[reversed_map_all_poss_states[(agent,dealer,0)]]
+
 
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
-    
-    for i in range(1000):
-        print(all_possible_states[i] , "   :  " ,visits_per_state[i])
-    agent = np.arange(10,22)
-    dealer = np.arange(0,12)      
+    # for i in range(1000):
+    #     print(all_possible_states[i] , "   :  " ,visits_per_state[i])
+    agent = np.arange(11,22)
+    dealer = np.arange(1,12)      
     agent, dealer = np.meshgrid( dealer,agent)
-    ax.plot_surface( dealer,agent, value, cmap='cividis')
+    ax.plot_surface( dealer,agent, value_pi, cmap='cividis')
+    plt.show()
+    ax.plot_surface( dealer,agent, value_Vs, cmap='cividis')
     plt.show()
 
-    for agent in range(10,22):
-        for dealer in range(0,12):
-            if value[agent-10][dealer] > 0: 
-                value[agent-10][dealer] =  1
-            elif value[agent-10][dealer] == 0 : 
-                value[agent-10][dealer] =  0
-            else:  value[agent-10][dealer] =  -1
 
-    agent = np.arange(10,22)
-    dealer = np.arange(0,12)      
-    agent, dealer = np.meshgrid(agent, dealer)
+    # for agent in range(10,22):
+    #     for dealer in range(0,12):
+    #         if value[agent-10][dealer] > 0: 
+    #             value[agent-10][dealer] =  1
+    #         elif value[agent-10][dealer] == 0 : 
+    #             value[agent-10][dealer] =  0
+    #         else:  value[agent-10][dealer] =  -1
+
+    # agent = np.arange(10,22)
+    # dealer = np.arange(0,12)      
+    # agent, dealer = np.meshgrid(agent, dealer)
     
-    ax.plot_surface(dealer,agent, value, cmap='cividis')
-    plt.show()
+    # ax.plot_surface(dealer,agent, value, cmap='cividis')
+    # plt.show()
